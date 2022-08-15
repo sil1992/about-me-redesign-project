85719235a936c8aec8da1bbe3673b1596797ec04
sil1992 <ehddhksdlsms@gmail.com>
2022-08-16 03:01:08 +0900

    220815_1

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ $("#search").focusin(function(){
 $("#search").focusout(function(){
   $(".input-box__placeholder > label").removeClass("focus");
 });
+
+
+let SliderBox1__i = 0;
+setInterval(function(){
+  $('.slider-box-1').attr('data-index',SliderBox1__i++ % 6);
+}, 3000);
